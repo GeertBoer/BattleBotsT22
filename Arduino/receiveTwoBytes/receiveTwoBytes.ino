b1d2b1694ef098af75c7f3512326c67a94bbf920
Bram de Wit <bram_de_wit@live.nl>
2016-11-21 14:53:00 +0100

    Update

--- Arduino/receiveTwoBytes/receiveTwoBytes.ino
+++ Arduino/receiveTwoBytes/receiveTwoBytes.ino
@@ -1,8 +1,18 @@
 byte byteArray[3];
+int buttonHit = 6;
+int buttonGotHit = 7;
+int ledAttack = 12;
+int ledSpeedRight = 11;
+int ledSpeedLeft = 10;
+int ledDirection = 9;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(buttonHit, INPUT);
+  pinMode(buttonGotHit, INPUT);
+  pinMode(ledDirection, OUTPUT);
+  pinMode(ledAttack, OUTPUT);
 }
 
 void loop() {
