838a8af47f8ad63ba9cf177a9524f2b9af76ef3d
Bram de Wit <bram_de_wit@live.nl>
2016-11-21 14:25:42 +0100

    Update
    Nieuwe basis van arduino code

--- Arduino/receiveTwoBytes/receiveTwoBytes.ino
+++ Arduino/receiveTwoBytes/receiveTwoBytes.ino
@@ -1,17 +1,14 @@
-byte byteArray[2];
+byte byteArray[3];
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(9, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0)
   {
-    Serial.readBytes(byteArray, 2);
-    analogWrite(9, byteArray[0]);
-    analogWrite(10, byteArray[1]);
+    Serial.readBytes(byteArray, 3);
   }
 }
