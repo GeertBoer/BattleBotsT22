4bcdf669d58d8a84471dddd86e070304437310cc
SealRut <marijnrutten@live.nl>
2016-11-24 11:01:06 +0100

    Arduino Update
    Update

--- Arduino/receiveTwoBytes/receiveTwoBytes.ino
+++ Arduino/receiveTwoBytes/receiveTwoBytes.ino
@@ -1,8 +1,23 @@
 #include "Rp6.h"
+
+#define stepsPerRev 2048//4096
+#define attackSteps 700
+#define recoverSteps 500
+
 byte byteArray[3];
 RP6_DIRECTION dir = RP6_FORWARD;
-int buttonHit = 6;
-int buttonGotHit = 7;
+const int buttonHit = 8;
+const int buttonGotHit = 9;
+
+const int blue = 10;
+const int pink = 11;
+const int yellow = 12;
+const int orange = 13;
+
+bool attack = false;
+int steps = 0;
+int stepsLeft = 0;
+unsigned long lastTick;
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,6 +25,15 @@ void setup() {
   Rp6.begin();
   pinMode(buttonHit, INPUT);
   pinMode(buttonGotHit, INPUT);
+
+  pinMode(blue, OUTPUT);
+  pinMode(pink, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(orange, OUTPUT);
+
+  //start one attack
+  attack = true;
+  stepsLeft = attackSteps;
 }
 
 void loop() {
@@ -31,21 +55,90 @@ void loop() {
     
     if(byteArray[0] & 2)
     {
-      Attack(byteArray[0]);
+      Serial.println("Hier");
+      AttackTick();
     }
   }
-  if(buttonHit || buttonGotHit)
+  if(digitalRead(buttonHit) || digitalRead(buttonGotHit))
   {
     //Marijn moet hier even zorgen dat dit if werken als hij wordt geraakt of raakt
     byte respond = 0;
-    if(buttonHit)
+    if(digitalRead(buttonHit))
     {
       respond |= 2;
     }
-    if(buttonGotHit)
+    if(digitalRead(buttonGotHit))
     {
       respond |= 1;
     }
     Serial.println(respond);
+  }
+}
+
+void AttackTick()
+{
+  if (stepsLeft >= 0)
+  {
+    if (micros() - lastTick >= 1000)
+    {
+      stepperTick(attack);
+      lastTick = micros();
+      stepsLeft--;
+    }
+  }
+  else if (attack)
+  {
+    attack = false;
+    stepsLeft = recoverSteps;
+  }
+}
+
+void stepperTick(bool dir) {
+  switch (steps)
+  {
+    case 0:
+      digitalWrite(blue, HIGH);
+      digitalWrite(pink, HIGH);
+      digitalWrite(yellow, LOW);
+      digitalWrite(orange, LOW);
+      delay(3);
+      break;
+    case 1:
+      digitalWrite(blue, LOW);
+      digitalWrite(pink, HIGH);
+      digitalWrite(yellow, HIGH);
+      digitalWrite(orange, LOW);
+      delay(3);
+      break;
+    case 2:
+      digitalWrite(blue, LOW);
+      digitalWrite(pink, LOW);
+      digitalWrite(yellow, HIGH);
+      digitalWrite(orange, HIGH);
+      delay(3);
+      break;
+    case 3:
+      digitalWrite(blue, HIGH);
+      digitalWrite(pink, LOW);
+      digitalWrite(yellow, LOW);
+      digitalWrite(orange, HIGH);
+      delay(3);
+      break;
+  }
+  if (!dir)
+  {
+    steps++;
+    if (steps > 3)
+    {
+      steps = 0;
+    }
+  }
+  else
+  {
+    steps--;
+    if (steps < 0)
+    {
+      steps = 3;
+    }
   }
 }
